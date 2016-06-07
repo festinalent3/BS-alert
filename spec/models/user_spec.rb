3fdae87ba248f333dfb71bb59df2d62d340811dd
DXTimer <ibbakalov@gmail.com>
2016-06-08 00:46:00 +0100

    Added user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, :type => :model do
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  it { expect(subject).to respond_to(:email) }
+  it { expect(subject).to respond_to(:password) }
+  it { expect(subject).to respond_to(:password_confirmation) }
+
+  it { expect(subject).to be_valid }
 end

e15df7c7080838056bff50e3dc9cd9eac574d728
DXTimer <ibbakalov@gmail.com>
2016-06-08 10:47:35 +0100

    Views for User Create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,25 @@
 class Api::V1::UsersController < ApplicationController
-  before_filter :authenticate_user!, except: [:show]
+  # before_filter :authenticate_user!, except: [:show]
   respond_to :json
 
   def show
     @user = User.find(params[:id])
     respond_with @user
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      return render :layout => false, json: { user: user }, status: 201, location: [:api, user]
+    else
+      return render :layout => false, json: { error: user.errors }, status: 422
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-	    resources :users, :only => [:show]
+	    resources :users, :only => [:show, :create]
     end
   end
   root to: 'users#index'
